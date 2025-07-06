2d3c7257065c9d471b30d2e9e0565e2c78df9853
RYO WATANABE <wrhobbyshumi123@gmail.com>
2025-07-06 23:31:23 +0900

    fix confirm message

--- src/scripts/secret.js
+++ src/scripts/secret.js
@@ -4,9 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const body = document.body;
 
     acceptButton.addEventListener("click", () => {
-        body.className = "accept-background"; // 背景を変更
-        const actionsDiv = document.querySelector(".actions");
-        actionsDiv.innerHTML = "<h2>渡邉 凌が彼氏になりました</h2><h2>山下 真莉佳は彼女になりました</h2><h2>これからよろしくお願いします。</h2>"; // メッセージを表示
+        const confirmation = confirm("本当に告白を受け入れてもよろしいですか？");
+        if (confirmation) {
+            body.className = "accept-background"; // 背景を変更
+            const actionsDiv = document.querySelector(".actions");
+            actionsDiv.innerHTML = `
+                <h2>渡邉 凌が彼氏になりました</h2>
+                <h2>山下 真莉佳は彼女になりました</h2>
+                <h2>これからよろしくお願いします。</h2>
+            `; // メッセージを表示
+        }
     });
 
     declineButton.addEventListener("click", () => {
